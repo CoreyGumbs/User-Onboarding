9de87a8c28d7239eb43b97abf699fdcd44144f95
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-14 23:56:20 -0500

    [Add] created Form code & imported Form Component to App component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,7 +4,21 @@ import React from 'react';
 const Form = () => {
     return(
         <div>
-
+            <form action="">
+                <label htmlFor="name">
+                    Name: <input name="name" type="text"/>
+                </label>
+                <label htmlFor="email">
+                    Email: <input name="email" type="email"/>
+                </label>
+                <label htmlFor="password">
+                    Password: <input name="password" type="password"/>
+                </label>
+                <label htmlFor="tos">
+                    Terms of Service: <input name="tos" type="checkbox" id=""/>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
