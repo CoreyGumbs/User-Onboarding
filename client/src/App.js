e589dfac40ddd03f491b7d2d1eae3fe165169709
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-15 10:05:48 -0500

    [Add] added withFormik HOC and values

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form/Form';
+import FormikForm from './components/Form/Form';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <FormikForm />
     </div>
   );
 }
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import {withFormik} from 'formik';
 
 
-const Form = () => {
+const Form = ({values, handleChange}) => {
     return(
         <div>
             <form action="">
                 <label htmlFor="name">
-                    Name: <input name="name" type="text"/>
+                    Name: <input name="name" type="text" value={values.name} onChange={handleChange}/>
                 </label>
                 <label htmlFor="email">
-                    Email: <input name="email" type="email"/>
+                    Email: <input name="email" type="email" values={values.email} onChange={handleChange}/>
                 </label>
                 <label htmlFor="password">
-                    Password: <input name="password" type="password"/>
+                    Password: <input name="password" type="password" values={values.password} onChange={handleChange}/>
                 </label>
-                <label htmlFor="tos">
-                    Terms of Service: <input name="tos" type="checkbox" id=""/>
+                <label htmlFor="terms">
+                    Terms of Service: <input name="terms" type="checkbox" id=""/>
                 </label>
                 <button type="submit">Submit</button>
             </form>
@@ -23,4 +24,17 @@ const Form = () => {
     )
 }
 
-export default Form;
+const FormikForm = withFormik({
+    mapPropsToValues({name, email, password, terms}){
+        return {
+            name: name || "Enter Your Name",
+            email: email ||  "",
+            password: password || "",
+            terms: false
+        }
+    }
+})(Form);
+
+export default FormikForm;
+
+console.log(FormikForm);
